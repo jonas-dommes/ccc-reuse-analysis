99db17c090832932aaa29e4542d10feb12ba00d3
jonas-dommes <jonas.dommes@gmail.com>
2021-09-29 15:02:52 +0200

    simple one dimensional copy kernel

--- test-kernels/copy/copy.cu
+++ test-kernels/copy/copy.cu
@@ -4,13 +4,13 @@
 * modification, are permitted provided that the following conditions
 * are met:
 *  * Redistributions of source code must retain the above copyright
-*    notice, this list of conditions and the following disclaimer.
+*	notice, this list of conditions and the following disclaimer.
 *  * Redistributions in binary form must reproduce the above copyright
-*    notice, this list of conditions and the following disclaimer in the
-*    documentation and/or other materials provided with the distribution.
+*	notice, this list of conditions and the following disclaimer in the
+*	documentation and/or other materials provided with the distribution.
 *  * Neither the name of NVIDIA CORPORATION nor the names of its
-*    contributors may be used to endorse or promote products derived
-*    from this software without specific prior written permission.
+*	contributors may be used to endorse or promote products derived
+*	from this software without specific prior written permission.
 *
 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
 * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -28,42 +28,131 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define NUM_REPS 1
+
 // Convenience function for checking CUDA runtime API results
 // can be wrapped around any runtime API call. No-op in release builds.
 inline cudaError_t checkCuda(cudaError_t result) {
-    #if defined(DEBUG) || defined(_DEBUG)
-    if (result != cudaSuccess) {
-        fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
-        assert(result == cudaSuccess);
-    }
-    #endif
-    return result;
+	#if defined(DEBUG) || defined(_DEBUG)
+	if (result != cudaSuccess) {
+		fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
+		assert(result == cudaSuccess);
+	}
+	#endif
+	return result;
 }
 
 
 // Check errors and print GB/s
 void postprocess(const float *ref, const float *res, int n, float ms) {
-    bool passed = true;
-    for (int i = 0; i < n; i++) {
-        if (res[i] != ref[i]) {
-            printf("%d %f %f\n", i, res[i], ref[i]);
-            printf("%25s\n", "*** FAILED ***");
-            passed = false;
-            break;
-        }
-    }
-    if (passed) {
-        printf("%20.2f\n", 2 * n * sizeof(float) * 1e-6 * NUM_REPS / ms );
-    }
+	bool passed = true;
+	for (int i = 0; i < n; i++) {
+		if (res[i] != ref[i]) {
+			printf("%d %f %f\n", i, res[i], ref[i]);
+			printf("%25s\n", "*** FAILED ***");
+			passed = false;
+			break;
+		}
+	}
+	if (passed) {
+		printf("Time in ms: %6.3f\n", ms );
+		// printf("%20.2f\n", 2 * n * sizeof(float) * 1e-6 * NUM_REPS / ms );
+	}
 }
 
-// Kernel to copy 1 dimensional array
-__global__ void copy_array(float *odata, const float *idata, int work_per_thread, int stride) {
-    int id =
+#define PRINT_IDX 132
+/* Kernel to copy 1 dimensional array
+*  int work_per_thread  number of datapoints each thread works
+*/
+__global__ void copy_array(float *odata, const float *idata, int work_per_thread) {
+
+	int tid = blockIdx.x * blockDim.x * work_per_thread + threadIdx.x;
+	// printf("%2d tid: %d\n", blockIdx.x, tid);
+
+	// if(tid == PRINT_IDX) printf("blockIdx.x=%d   blockDim.x=%d   gridDim.x=%d   threadIdx.x=%d \n", blockIdx.x, blockDim.x, gridDim.x, threadIdx.x);
+
+	// if(tid == PRINT_IDX) printf("idata[%d]: %f\n", PRINT_IDX, idata[PRINT_IDX]);
+
+	for (int i = 0; i < work_per_thread; i++) {
+		int index = i * blockDim.x + tid;
+		odata[index] = idata[index];
+
+		// if(tid == PRINT_IDX) printf("i: %d   index: %d\n", i, index);
+
+	}
+
+	// if(tid == PRINT_IDX) printf("odata[%d]: %f\n", PRINT_IDX, odata[PRINT_IDX]);
 }
 
-
+void print_args(int argc, char **argv) {
+	if (argc != 4) {
+		printf("Error: Format should be: ./copy num_blocks threads_per_block work_per_thread \n");
+		exit(1);
+	} else {
+		printf("num_blocks        = %d\n", atoi(argv[1]));
+		printf("threads_per_block = %d\n", atoi(argv[2]));
+		printf("work_per_thread   = %d\n", atoi(argv[3]));
+	}
+}
 
 int main(int argc, char **argv) {
 
+	// Handle and print arguments
+	print_args(argc, argv);
+	int num_blocks = atoi(argv[1]);
+	int threads_per_block = atoi(argv[2]);
+	int work_per_thread = atoi(argv[3]);
+
+	int data_size = num_blocks * threads_per_block * work_per_thread;
+	printf("data_size = %d\n", data_size);
+	printf("num_threads = %d\n", num_blocks * threads_per_block);
+
+	// Prepare Kernel dimensions
+	dim3 dimGrid(num_blocks, 1, 1);
+	dim3 dimBlock(threads_per_block, 1, 1);
+
+	// Prepare host data
+	float *h_idata = (float*) calloc(data_size, sizeof(float));
+	float *h_odata = (float*) calloc(data_size, sizeof(float));
+	float *original_idata = (float*) calloc(data_size, sizeof(float));
+
+	srand(42);
+
+	for (int i = 0; i < data_size; i++) {
+		h_idata[i] = ((float) rand()/(float) (RAND_MAX)) * 10;
+		original_idata[i] = h_idata[i];
+	}
+
+	// Prepare device data
+	float *d_idata, *d_odata;
+	checkCuda(cudaMalloc(&d_idata, data_size * sizeof(float)));
+	checkCuda(cudaMalloc(&d_odata, data_size * sizeof(float)));
+	checkCuda(cudaMemcpy(d_idata, h_idata, data_size * sizeof(float), cudaMemcpyHostToDevice));
+	checkCuda(cudaMemset(d_odata, 0, data_size * sizeof(float)));
+
+	// Events for timing
+	cudaEvent_t startEvent, stopEvent;
+	checkCuda(cudaEventCreate(&startEvent));
+	checkCuda(cudaEventCreate(&stopEvent));
+	float ms;
+
+	// Run Kernel
+	checkCuda(cudaEventRecord(startEvent, 0));
+	copy_array<<<dimGrid, dimBlock>>>(d_odata, d_idata, work_per_thread);
+	checkCuda(cudaEventRecord(stopEvent, 0));
+    checkCuda(cudaEventSynchronize(stopEvent));
+    checkCuda(cudaEventElapsedTime(&ms, startEvent, stopEvent));
+	checkCuda(cudaMemcpy(h_odata, d_odata, data_size * sizeof(float), cudaMemcpyDeviceToHost));
+
+	// Analyse
+	postprocess(original_idata, h_odata, data_size, ms);
+
+	// Cleanup
+	checkCuda(cudaEventDestroy(startEvent));
+	checkCuda(cudaEventDestroy(stopEvent));
+	checkCuda(cudaFree(d_idata));
+	checkCuda(cudaFree(d_odata));
+	free(h_idata);
+	free(h_odata);
+	free(original_idata);
 }
