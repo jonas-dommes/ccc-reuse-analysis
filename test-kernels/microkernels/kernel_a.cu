76f2f1417feb205b1253aac79b3b8f9676e9e586
jonas-dommes <jonas.dommes@gmail.com>
2021-10-21 12:57:41 +0200

    finish kernel a

--- test-kernels/microkernels/kernel_a.cu
+++ test-kernels/microkernels/kernel_a.cu
@@ -27,6 +27,10 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <time.h>
+
+#define DEBUG 1
+#define eps 10e-6
 
 __global__ void kernel_a(float *odata, const float *idata, int work_per_thread) {
 
@@ -38,11 +42,10 @@ __global__ void kernel_a(float *odata, const float *idata, int work_per_thread) 
 	}
 
 	if (offset < 32) {
-		odata[offset] = -odata[offset] // TODO: *(-1)?
+		odata[offset] = -odata[offset]; // TODO: *(-1)?
 		// Alternative: Reduce to one datapoint?
 	}
 }
-
 
 
 // Convenience function for checking CUDA runtime API results
@@ -62,12 +65,13 @@ inline cudaError_t checkCuda(cudaError_t result) {
 int check_result(const float *reference, const float *result, int n) {
 
 	for (int i = 0; i < n; i++) {
-		if (reference[i] != result[i]) {
-			printf("Wrong result: reference[%d] = %f\t result[%d] = \n\n", i, reference[i], i, result[i]);
-			return 1;
+		if (abs(reference[i] - result[i]) > eps) {
+			printf("Wrong result: reference[%d] = %.20f\n", i, reference[i]);
+			printf("Wrong result:    result[%d] = %.20f\n",i, result[i]);
+			return i;
 		}
 	}
-	return 0;
+	return -1;
 }
 
 void print_args(int argc, char **argv) {
@@ -109,17 +113,65 @@ int main(int argc, char **argv) {
 	dim3 dimBlock(threads_per_block, 1, 1);
 
 	// Prepare host data structures
+	float *h_idata = (float*) calloc(data_points, sizeof(float));
+	float *h_odata = (float*) calloc(data_points, sizeof(float));
+	float *reference = (float*) calloc(data_points, sizeof(float));
 
 	// Initiallize input array
+	init_random(h_idata, data_points);
+
+	// Calculate reference
+	clock_t begin = clock();
+
+	for (int i = 0; i < data_points; i++) {
+		reference[i] = h_idata[i] * h_idata[i] - 1;
+	}
+
+	for (int i = 0; i < 32; i++) {
+		reference[i] = -reference[i]; // TODO: *(-1)?
+		// Alternative: Reduce to one datapoint?
+	}
+
+	clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC * 1000;
+	printf("Calculated reference in %.5f ms\n", time_spent);
 
 	// Prepare device data structures
+	float *d_idata, *d_odata;
+	checkCuda(cudaMalloc(&d_idata, data_points * sizeof(float)));
+	checkCuda(cudaMalloc(&d_odata, data_points * sizeof(float)));
+	checkCuda(cudaMemcpy(d_idata, h_idata, data_points * sizeof(float), cudaMemcpyHostToDevice));
+	checkCuda(cudaMemset(d_odata, 0, data_points * sizeof(float)));
 
 	// Events for timing
+	cudaEvent_t startEvent, stopEvent;
+	checkCuda(cudaEventCreate(&startEvent));
+	checkCuda(cudaEventCreate(&stopEvent));
+	float ms;
 
 	// Run Kernel
+	checkCuda(cudaEventRecord(startEvent, 0));
+	kernel_a<<<dimGrid, dimBlock>>>(d_odata, d_idata, work_per_thread);
+	checkCuda(cudaGetLastError());
+	checkCuda(cudaEventRecord(stopEvent, 0));
+	checkCuda(cudaEventSynchronize(stopEvent));
+	checkCuda(cudaEventElapsedTime(&ms, startEvent, stopEvent));
+	checkCuda(cudaMemcpy(h_odata, d_odata, data_points * sizeof(float), cudaMemcpyDeviceToHost));
 
 	// Analyse
+	int is_correct = check_result(reference, h_odata, data_points);
+	if (is_correct != -1) {
+		printf("Wrong result:    h_idata[%d] = %.20f\n\n",is_correct, h_idata[is_correct]);
+	} else {
+		printf("Correct result after %.5f ms\n", ms);
+	}
 
 	// Cleanup
-
+	checkCuda(cudaEventDestroy(startEvent));
+	checkCuda(cudaEventDestroy(stopEvent));
+	checkCuda(cudaFree(d_idata));
+	checkCuda(cudaFree(d_odata));
+	free(h_idata);
+	free(h_odata);
+	free(reference);
 }
