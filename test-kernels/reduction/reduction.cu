f9d0dd598d3cd801b9e0961e6b2f85ddf57648ff
jonas-dommes <jonas.dommes@gmail.com>
2021-11-11 16:07:16 +0100

    reduce 1 completed

--- test-kernels/reduction/reduction.cu
+++ test-kernels/reduction/reduction.cu
@@ -31,7 +31,6 @@
 
 #include "utility.h"
 
-#define DEBUG 1
 
 // Naive reduce (interleaved addressing)
 __global__ void reduce1(int *d_idata, int *d_odata) {
@@ -55,101 +54,93 @@ __global__ void reduce1(int *d_idata, int *d_odata) {
 	// write result for this block to global mem
 	if (tid == 0)  {
 		d_odata[blockIdx.x] = sdata[0];
-
-		// Recursively call kernel
-		if (blockIdx.x == 0) {
-			if (gridDim.x >= blockDim.x) {
-				reduce1<<<gridDim.x / blockDim.x, blockdim.x>>>(d_idata, d_odata);
-			} else if (gridDim.x > 1) {
-				reduce1<<<1, gridDim.x>>>(d_idata, d_odata);
-			}
-		}
 	}
-
-
-// strided indexing for non-divergent branching (interleaved addressing) --> Bank conflicts
-__global__ void reduce2(int *d_idata, int *d_odata) {
-
-	extern __shared__ int sdata[];
-
-	// each thread loads one element from global to shared mem
-	unsigned int tid = threadIdx.x;
-	unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-	sdata[tid] = d_idata[i];
-	__syncthreads();
-
-	// do reduction in shared mem
-	for (unsigned int s=1; s < blockDim.x; s *= 2)  {
-		int index = 2 * s * tid;
-		if (index < blockDim.x) {
-			sdata[index] += sdata[index + s];
-		}
-		__syncthreads();
-	}
-
-	// write result for this block to global mem
-	if (tid == 0) d_odata[blockIdx.x] = sdata[0];
 }
 
-
-// Sequential Addressing
-__global__ void reduce3(int *d_idata, int *d_odata) {
-
-	extern __shared__ int sdata[];
-
-	// each thread loads one element from global to shared mem
-	unsigned int tid = threadIdx.x;
-	unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-	sdata[tid] = d_idata[i];
-	__syncthreads();
-
-	// do reduction in shared mem
-	for (unsigned int s=blockDim.x/2; s>0; s>>=1) {
-		if (tid < s) {
-			sdata[tid] += sdata[tid + s];
-		}
-		__syncthreads();
-	}
-
-	// write result for this block to global mem
-	if (tid == 0) d_odata[blockIdx.x] = sdata[0];
-}
-
-
-__device__ void warpReduce(volatile int* sdata, int tid) {
-	sdata[tid] += sdata[tid + 32];
-	sdata[tid] += sdata[tid + 16];
-	sdata[tid] += sdata[tid +  8];
-	sdata[tid] += sdata[tid +  4];
-	sdata[tid] += sdata[tid +  2];
-	sdata[tid] += sdata[tid +  1];
-}
-
-
-// Unroll last Warp
-__global__ void reduce4(int *d_idata, int *d_odata) {
-
-	extern __shared__ int sdata[];
-
-	// perform first level of reduction,
-	// reading from global memory, writing to shared memory
-	unsigned int tid = threadIdx.x;
-	unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-	sdata[tid] = d_idata[i];
-	__syncthreads();
-
-	// do reduction in shared mem
-	for (unsigned int s=blockDim.x/2; s>32; s>>=1) {
-		if (tid < s)
-		sdata[tid] += sdata[tid + s];
-		__syncthreads();
-	}
-
-	if (tid < 32) warpReduce(sdata, tid);
-
-	// write result for this block to global mem
-	if (tid == 0) d_odata[blockIdx.x] = sdata[0];
-}
+//
+// // strided indexing for non-divergent branching (interleaved addressing) --> Bank conflicts
+// __global__ void reduce2(int *d_idata, int *d_odata) {
+//
+// 	extern __shared__ int sdata[];
+//
+// 	// each thread loads one element from global to shared mem
+// 	unsigned int tid = threadIdx.x;
+// 	unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
+// 	sdata[tid] = d_idata[i];
+// 	__syncthreads();
+//
+// 	// do reduction in shared mem
+// 	for (unsigned int s=1; s < blockDim.x; s *= 2)  {
+// 		int index = 2 * s * tid;
+// 		if (index < blockDim.x) {
+// 			sdata[index] += sdata[index + s];
+// 		}
+// 		__syncthreads();
+// 	}
+//
+// 	// write result for this block to global mem
+// 	if (tid == 0) d_odata[blockIdx.x] = sdata[0];
+// }
+//
+//
+// // Sequential Addressing
+// __global__ void reduce3(int *d_idata, int *d_odata) {
+//
+// 	extern __shared__ int sdata[];
+//
+// 	// each thread loads one element from global to shared mem
+// 	unsigned int tid = threadIdx.x;
+// 	unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
+// 	sdata[tid] = d_idata[i];
+// 	__syncthreads();
+//
+// 	// do reduction in shared mem
+// 	for (unsigned int s=blockDim.x/2; s>0; s>>=1) {
+// 		if (tid < s) {
+// 			sdata[tid] += sdata[tid + s];
+// 		}
+// 		__syncthreads();
+// 	}
+//
+// 	// write result for this block to global mem
+// 	if (tid == 0) d_odata[blockIdx.x] = sdata[0];
+// }
+//
+//
+// __device__ void warpReduce(volatile int* sdata, int tid) {
+// 	sdata[tid] += sdata[tid + 32];
+// 	sdata[tid] += sdata[tid + 16];
+// 	sdata[tid] += sdata[tid +  8];
+// 	sdata[tid] += sdata[tid +  4];
+// 	sdata[tid] += sdata[tid +  2];
+// 	sdata[tid] += sdata[tid +  1];
+// }
+//
+//
+// // Unroll last Warp
+// __global__ void reduce4(int *d_idata, int *d_odata) {
+//
+// 	extern __shared__ int sdata[];
+//
+// 	// perform first level of reduction,
+// 	// reading from global memory, writing to shared memory
+// 	unsigned int tid = threadIdx.x;
+// 	unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
+// 	sdata[tid] = d_idata[i];
+// 	__syncthreads();
+//
+// 	// do reduction in shared mem
+// 	for (unsigned int s=blockDim.x/2; s>32; s>>=1) {
+// 		if (tid < s)
+// 		sdata[tid] += sdata[tid + s];
+// 		__syncthreads();
+// 	}
+//
+// 	if (tid < 32) warpReduce(sdata, tid);
+//
+// 	// write result for this block to global mem
+// 	if (tid == 0) d_odata[blockIdx.x] = sdata[0];
+// }
 
 // // First Add during load and unroll last Warp
 // __global__ void reduce4(int *d_idata, int *d_odata) {
@@ -176,48 +167,68 @@ __global__ void reduce4(int *d_idata, int *d_odata) {
 // 	if (tid == 0) d_odata[blockIdx.x] = sdata[0];
 // }
 
-
 int main(int argc, char **argv) {
 
-	int num_kernels = 1;
-	int num_runs = 1;
-	int num_blockdims = 1;
-	int data_points[num_runs] = {1024}; // biggest last
-	int blockdims[num_blockdims] = {1024};
-	// int data_points[num_runs] = {2097152}; // biggest last
-	// int blockdims[num_blockdims] = {128};
+	int success = 0;
 
-	// int num_kernels = 4;
-	// int num_runs = 3;
-	// int num_blockdims = 4;
-	// int data_points[num_runs] = {131072, 1048576, 2097152}; // biggest last
-	// int blockdims[num_blockdims] = {128, 356, 512, 1024};
+	// int num_kernels = 1;
+	// int num_runs = 1;
+	// int num_blockdims = 1;
+	// // int data_points[] = {1024}; // biggest last
+	// // int blockdims[] = {1024};
+	// int data_points[] = {2097152}; // biggest last
+	// int blockdims[] = {128};
+
+	int num_kernels = 1;
+	int num_runs = 3;
+	int num_blockdims = 4;
+	int data_points[] = {131072, 1048576, 2097152}; // biggest last
+	int blockdims[] = {128, 256, 512, 1024};
+	int ref[num_runs];
 
 	// Prepare host data
 	int *h_idata = (int*) calloc(data_points[num_runs-1], sizeof(int));
 	int *h_odata = (int*) calloc(data_points[num_runs-1], sizeof(int));
 	init_random_int(h_idata, data_points[num_runs-1]);
 
-	// Calculate reference
-	int sum = calc_reference_reduce(h_idata, data);
+
 
 	for (size_t i = 0; i < num_runs; i++) {
+
+		// Calculate reference
+		ref[i] = calc_reference_reduce(h_idata, data_points[i]);
+		printf("data_points = %d\n", data_points[i]);
+
 		for (size_t j = 0; j < num_blockdims; j++) {
 
-			// Prepare Kernel dimensions
-			dim3 dimGrid(data_points[i] / blockdims[j], 1, 1);
-			dim3 dimBlock(blockdims[j], 1, 1);
+			printf("## blockdims = %d\n", blockdims[j]);
 
 			// Prepare device data structures
-			float *d_idata, *d_odata;
+			int *d_idata, *d_odata;
 			checkCuda(cudaMalloc(&d_idata, data_points[i] * sizeof(int)));
 			checkCuda(cudaMalloc(&d_odata, data_points[i] * sizeof(int)));
 			checkCuda(cudaMemcpy(d_idata, h_idata, data_points[i] * sizeof(int), cudaMemcpyHostToDevice));
 			checkCuda(cudaMemset(d_odata, 0, data_points[i] * sizeof(int)));
 
-			reduce1<<<dimGrid, dimBlock>>>(d_idata, d_odata);
-			checkCuda(cudaGetLastError());
+			// Prepare Kernel dimensions
+			unsigned int num_data = data_points[i];
+			unsigned int dimBlock = blockdims[j];
 
+			while (num_data > dimBlock) { // Never ends if dimblock == 1
+				printf("#### Call reduce1<<<%d, %d>>>\n", num_data / dimBlock, dimBlock);
+				reduce1<<<num_data / dimBlock , dimBlock, dimBlock * sizeof(int)>>>(d_idata, d_odata);
+				checkCuda(cudaMemcpy(d_idata, d_odata, data_points[i] * sizeof(int), cudaMemcpyDeviceToDevice));
+				checkCuda(cudaGetLastError());
+				// checkCuda(cudaDeviceSynchronize());
+				num_data = num_data / dimBlock;
+			}
+
+			// Make last reduce (would be more efficient to do on Host)
+			if (num_data > 1) {
+				printf("#### Call reduce1<<<%d, %d>>> for last reduce\n", 1, num_data);
+				reduce1<<<1 , num_data, num_data * sizeof(int)>>>(d_idata, d_odata);
+				checkCuda(cudaGetLastError());
+			}
 
 			// reduce2<<<dimGrid, dimBlock>>>(d_idata, d_odata);
 			// checkCuda(cudaGetLastError());
@@ -228,19 +239,30 @@ int main(int argc, char **argv) {
 			// reduce4<<<dimGrid, dimBlock>>>(d_idata, d_odata);
 			// checkCuda(cudaGetLastError());
 
-
 			checkCuda(cudaMemcpy(h_odata, d_odata, data_points[i] * sizeof(int), cudaMemcpyDeviceToHost));
 
-			if (sum != h_odata[0]) {
-				printf("Reference= %d\nResult   = %d", sum, h_odata[0]);
+			if (ref[i] != h_odata[0]) {
+				printf("#### Reference= %d\nResult   = %d\n", ref[i], h_odata[0]);
+				success ++;
+			} else {
+				printf("## Success for blockdims = %d\n", blockdims[j]);
 			}
+
+			memset(h_odata, 0, data_points[i] * sizeof(int));
 
 			checkCuda(cudaFree(d_idata));
 			checkCuda(cudaFree(d_odata));
+
+
 		}
 
-		// memset(h_odata, 0, data_points[i] * sizeof(int));
+
+		printf("Finished for data_points = %d\n", data_points[i]);
 	}
 	free(h_idata);
 	free(h_odata);
+
+	if (success != 0) {
+		printf("\nERROR: %d reductions failed!\n", success);
+	}
 }
