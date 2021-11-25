570bfd3125a2558c14f90ee861592c3943e39e02
jonas-dommes <jonas.dommes@gmail.com>
2021-11-25 12:33:32 +0100

    reformat

--- llvm-rpc-passes/MemAccessDescriptor.h
+++ llvm-rpc-passes/MemAccessDescriptor.h
@@ -9,28 +9,33 @@ using namespace std;
 
 class MemAccessDescriptor {
 
-  private:
-    void init(const int x, const int y, const int z);
+private:
+	void init(const int x, const int y, const int z);
 
-  public:
-    static int SIZE;
-    static int CACHE_SIZE;
-    bool isValue;
-    int value;
-    //bool hasDim[3] = {false, false, false};
-    int sizes [3] = {1, 1, 1};
-    std::vector<vector<vector<int>>> mad;
-    
-  public:
-    MemAccessDescriptor(int value);
-    MemAccessDescriptor(int dimension, int n);
-    MemAccessDescriptor(const int x, const int y, const int z);
-    MemAccessDescriptor(function<int(int, int)> f, MemAccessDescriptor &a, MemAccessDescriptor &b);
-    MemAccessDescriptor select(MemAccessDescriptor &a, MemAccessDescriptor &b);
-    bool hasDim(int d);
-    MemAccessDescriptor compute(function<int(int, int)> f, MemAccessDescriptor &op);
-    list<int> getMemAccesses(int warpSize, int align, int cacheLineSize, bool *fullCoalescing);
-    void print();
+public:
+	static int SIZE;
+	static int CACHE_SIZE;
+	bool isValue;
+	int value;
+	int sizes [3] = {1, 1, 1};
+	std::vector<vector<vector<int>>> mad;
+
+public:
+
+	MemAccessDescriptor(int value);
+	MemAccessDescriptor(int dimension, int n);
+	MemAccessDescriptor(const int x, const int y, const int z);
+	MemAccessDescriptor(function<int(int, int)> f, MemAccessDescriptor &a, MemAccessDescriptor &b);
+
+	MemAccessDescriptor select(MemAccessDescriptor &a, MemAccessDescriptor &b);
+
+	bool hasDim(int d);
+
+	MemAccessDescriptor compute(function<int(int, int)> f, MemAccessDescriptor &op);
+
+	list<int> getMemAccesses(int warpSize, int align, int cacheLineSize, bool *fullCoalescing);
+
+	void print();
 };
 
 #endif
