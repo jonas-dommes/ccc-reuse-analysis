31e188d4d44023128adfceb5aabd8e7a3c85454c
jonas-dommes <jonas.dommes@gmail.com>
2022-02-20 12:50:25 +0100

    maa test

--- maa-pass/MemoryAccessAnalysis.cpp
+++ maa-pass/MemoryAccessAnalysis.cpp
@@ -1,26 +1,34 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/InstIterator.h"
 
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
 using namespace llvm;
 
-namespace {
-struct Hello : public FunctionPass {
-  static char ID;
-  Hello() : FunctionPass(ID) {}
+struct mma : public FunctionPass {
+	static char ID;
+	mma() : FunctionPass(ID) {}
 
-  bool runOnFunction(Function &F) override {
-    errs() << "Hello: ";
-    errs().write_escaped(F.getName()) << '\n';
-    return false;
-  }
+	bool runOnFunction(Function &F) override {
+
+		unsigned int num_maas = 0;
+
+		for (auto &I : F) {
+			// errs() << I << "\n";
+			errs() << "I" << "\n";
+		}
+
+		// 	for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I)
+		// errs() << *I << "\n";
+
+
+
+		errs().write_escaped(F.getName()) << " uses " << num_maas << " memory access instructions\n";
+
+		return false;
+	}
 }; // end of struct Hello
-}  // end of anonymous namespace
 
-char Hello::ID = 0;
-static RegisterPass<Hello> X("hello", "Hello World Pass",
-                             true /* Only looks at CFG */,
-                             true /* Analysis Pass */);
+char mma::ID = 0;
+static RegisterPass<mma> X("mma", "Memory Access Analysis Pass", true /* Only looks at CFG */, true /* Analysis Pass */);
