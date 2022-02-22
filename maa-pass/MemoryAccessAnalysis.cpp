208548632818ea74dbc980a0001e1c96a69ee7c7
jonas-dommes <jonas.dommes@gmail.com>
2022-02-22 13:44:51 +0100

    add printing method

--- maa-pass/MemoryAccessAnalysis.cpp
+++ maa-pass/MemoryAccessAnalysis.cpp
@@ -1,11 +1,17 @@
+#include <string>
+
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include <llvm/IR/Instructions.h>
 #include "llvm/IR/InstIterator.h"
 #include "llvm/Support/raw_ostream.h"
 
+#include "MemoryAccessAnalysis.h"
 
 using namespace llvm;
+
+
+
 
 struct maa : public FunctionPass {
 	static char ID;
@@ -13,29 +19,40 @@ struct maa : public FunctionPass {
 
 	bool runOnFunction(Function &F) override {
 
-		unsigned int num_loads = 0;
-		unsigned int num_stores = 0;
+		struct pass_stats stats;
+		stats.function_name = F.getName();
 
-	for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
+		for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
 
-		if (isa<StoreInst>(*I)) {
+			if (isa<StoreInst>(*I)) {
 
-			num_stores++;
+				stats.num_stores++;
 
+			} else if (isa<LoadInst>(*I)) {
 
-
-		} else if (isa<LoadInst>(*I)) {
-
-			num_loads++;
+				stats.num_loads++;
+			}
 		}
+
+		print_stats(&stats);
+
+		return false;
 	}
 
 
-	errs().write_escaped(F.getName()) << " uses " << num_stores + num_loads << " memory access instructions\n";
+private:
 
-	return false;
-}
-}; // end of struct Hello
+	void print_stats(struct pass_stats *stats) {
+
+		printf("%s\n", stats->function_name.c_str());
+		printf("\tNum loads:\t%4d\n", stats->num_loads);
+		printf("\tNum stores:\t%4d\n", stats->num_stores);
+
+	}
+};
+
+
+
 
 char maa::ID = 0;
 static RegisterPass<maa> X("maa", "Memory Access Analysis Pass", true /* Only looks at CFG */, true /* Analysis Pass */);
