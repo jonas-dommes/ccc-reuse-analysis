ca3c013115dd7aba308eccdc57f32223dc692d91
jonas-dommes <jonas.dommes@gmail.com>
2022-05-02 12:18:23 +0200

    implement isKernel()

--- maa-pass/FunctionStats.cpp
+++ maa-pass/FunctionStats.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
 
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+
+#include "NVPTXUtilities.h"
+
+
+
+
 #include "FunctionStats.h"
+
+
+bool FunctionStats::isKernel(llvm::Function &F) {
+
+	bool isCUDA = F.getParent()->getTargetTriple() == CUDA_TARGET_TRIPLE;
+	bool isKernel = isKernelFunction(F);
+
+	if (!isCUDA || !isKernel) {
+		return false;
+	}
+
+	this->is_kernel = true;
+	return true;
+}
+
 
 void FunctionStats::printFunctionStats() {
 
-		printf("%s\n", this->function_name.c_str());
-		printf("\tNum loads  (unique): %2d (%2d)\n", this->num_loads, this->unique_loads);
-		printf("\tNum stores (unique): %2d (%2d)\n", this->num_stores, this->unique_stores);
-		printf("\tNum total  (unique): %2d (%2d)\n", this->num_stores + this->num_loads, this->unique_total);
+		printf("%s", this->function_name.c_str());
+
+		if (this->is_kernel) {
+			printf(" is kernel function\n");
+			printf("\tNum loads  (unique): %2d (%2d)\n", this->num_loads, this->unique_loads);
+			printf("\tNum stores (unique): %2d (%2d)\n", this->num_stores, this->unique_stores);
+			printf("\tNum total  (unique): %2d (%2d)\n", this->num_stores + this->num_loads, this->unique_total);
+		} else {
+			printf(" is NOT kernel function. No Analysis\n");
+
+		}
+
+
  }
--- maa-pass/FunctionStats.h
+++ maa-pass/FunctionStats.h
@@ -1,6 +1,9 @@
 #ifndef FUNCTIONSTATS_H
 #define FUNCTIONSTATS_H
 
+#define CUDA_TARGET_TRIPLE         "nvptx64-nvidia-cuda"
+
+
 class FunctionStats {
 public:
 	std::string function_name;
@@ -9,8 +12,12 @@ public:
 	unsigned int unique_loads = 0;
 	unsigned int unique_stores = 0;
 	unsigned int unique_total = 0;
+	bool is_kernel;
+
+	bool isKernel(llvm::Function &F);
 
 	void printFunctionStats();
+	void printInstrMap();
 };
 
 
