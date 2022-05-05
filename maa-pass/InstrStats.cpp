881ba6e83a9738a8d6039577e593da7eb281e1d4
jonas-dommes <jonas.dommes@gmail.com>
2022-05-05 13:43:24 +0200

    add printInstr()

--- maa-pass/InstrStats.cpp
+++ maa-pass/InstrStats.cpp
@@ -9,4 +9,13 @@
 
 void InstrStats::printInstrStats() {
 
+	if (this->is_load) {
+		printf("\t\tLoad Instr\n");
+	} else if (this->is_store) {
+		printf("\t\tStore Instr\n");
+	}
+
+	printf("\t\tLoop Depth: %d\n", this->loop_depth);
+	printf("\t\tAddr: %p\n", this->addr);
+
 }
--- maa-pass/InstrStats.h
+++ maa-pass/InstrStats.h
@@ -5,12 +5,13 @@ class InstrStats {
 public:
 	// bool is_loop;
 	unsigned int loop_depth = 0;   // 0 -> no loop
+	bool is_load = false;
+	bool is_store = false;
+
 	bool is_tid_dep = false;
 	bool is_bid_dep = false;
 	bool first_use = false;            // Addr is used here for the first time
 	llvm::Value * addr = NULL;
-	bool is_load = false;
-	bool is_store = false;
 
 	void printInstrStats();
 
