1eb9733a5cb3cedb9df60b9105213e4448a418fc
Jonas <jonas.dommes@gmail.com>
2022-08-01 13:58:11 +0200

    [WIP] TODO: see if this works with GPU

--- test-kernels/microkernels/microkernels.cu
+++ test-kernels/microkernels/microkernels.cu
@@ -26,14 +26,13 @@
 */
 
 /*
-clang++-9 -S -emit-llvm -O1 ~/ba/code/ccc-reuse-analysis/test-kernels/microkernels/kernels.cu --cuda-gpu-arch=sm_75 -fno-discard-value-names --cuda-device-only -o ~/ba/code/ccc-reuse-analysis/test-kernels/microkernels/kernels.ll
+clang++-9 -S -emit-llvm -O1 ~/ba/code/ccc-reuse-analysis/test-kernels/microkernels/microkernels.cu --cuda-gpu-arch=sm_75 -fno-discard-value-names --cuda-device-only -o ~/ba/code/ccc-reuse-analysis/test-kernels/microkernels/microkernels.ll
 */
 
 #include <stdio.h>
 #include <assert.h>
 #include <time.h>
 
-#include "utility.h"
 
 #define DEBUG 1
 #define TILE_DIM 32
@@ -181,8 +180,8 @@ __global__ void reduce3(int *d_data) {
 
 // simple copy kernel
 // Used as reference case representing best effective bandwidth.
-__global__ void copy(float *odata, const float *idata)
-{
+__global__ void copy(float *odata, const float *idata) {
+
 	int x = blockIdx.x * TILE_DIM + threadIdx.x;
 	int y = blockIdx.y * TILE_DIM + threadIdx.y;
 	int width = gridDim.x * TILE_DIM;
@@ -193,8 +192,8 @@ __global__ void copy(float *odata, const float *idata)
 // 2d simple transpose
 // Simplest transpose; doesn't use shared memory.
 // Global memory reads are coalesced but writes are not.
-__global__ void transposeSimple(float *odata, const float *idata)
-{
+__global__ void transposeSimple(float *odata, const float *idata) {
+
 	int col = blockIdx.x * blockDim.x + threadIdx.x;
 	int row = blockIdx.y * blockDim.y + threadIdx.y;
 	int width = gridDim.x * blockDim.x;
@@ -205,95 +204,4 @@ __global__ void transposeSimple(float *odata, const float *idata)
 
 	odata[indexOut]  = idata[indexIn];
 
-}
-
-
-int main(int argc, char **argv) {
-	//
-	// // Handle and print arguments
-	// print_args(argc, argv);
-	// int num_blocks = atoi(argv[1]);
-	// int threads_per_block = atoi(argv[2]);
-	// int work_per_thread = atoi(argv[3]);
-	//
-	// int data_points = num_blocks * threads_per_block * work_per_thread;
-	// printf("data_points = %d\n", data_points);
-	// printf("num_threads = %d\n", num_blocks * threads_per_block);
-	//
-	// // Prepare Kernel dimensions
-	// dim3 dimGrid(num_blocks, 1, 1);
-	// dim3 dimBlock(threads_per_block, 1, 1);
-	//
-	// // Prepare host data structures
-	// float *h_idata = (float*) calloc(data_points, sizeof(float));
-	// float *h_odata = (float*) calloc(data_points, sizeof(float));
-	//
-	// // Initiallize input array
-	// init_random(h_idata, data_points);
-	//
-	// // // Calculate reference
-	// // float *reference = (float*) calloc(data_points, sizeof(float));
-	// //
-	// // clock_t begin = clock();
-	// //
-	// // for (int i = 0; i < data_points; i++) {
-	// // 	reference[i] = h_idata[i] * h_idata[i] - 1;
-	// // 	reference[i] += reference[i] * h_idata[i] - 1;
-	// // }
-	// //
-	// // for (int i = 0; i < 32; i++) {
-	// // 	reference[i] = -reference[i];
-	// //
-	// // clock_t end = clock();
-	// // double time_spent = (double)(end - begin) / CLOCKS_PER_SEC * 1000;
-	// // printf("Calculated reference in %.5f ms\n", time_spent);
-	//
-	// // Prepare device data structures
-	// float *d_idata, *d_odata;
-	// checkCuda(cudaMalloc(&d_idata, data_points * sizeof(float)));
-	// checkCuda(cudaMalloc(&d_odata, data_points * sizeof(float)));
-	// checkCuda(cudaMemcpy(d_idata, h_idata, data_points * sizeof(float), cudaMemcpyHostToDevice));
-	// checkCuda(cudaMemset(d_odata, 0, data_points * sizeof(float)));
-	//
-	// // // Events for timing
-	// // cudaEvent_t startEvent, stopEvent;
-	// // checkCuda(cudaEventCreate(&startEvent));
-	// // checkCuda(cudaEventCreate(&stopEvent));
-	// // float ms;
-	//
-	// // Run Kernel a
-	// // checkCuda(cudaEventRecord(startEvent, 0));
-	// kernel_a<<<dimGrid, dimBlock>>>(d_odata, d_idata, work_per_thread);
-	// checkCuda(cudaGetLastError());
-	// // checkCuda(cudaEventRecord(stopEvent, 0));
-	// // checkCuda(cudaEventSynchronize(stopEvent));
-	// // checkCuda(cudaEventElapsedTime(&ms, startEvent, stopEvent));
-	// checkCuda(cudaMemcpy(h_odata, d_odata, data_points * sizeof(float), cudaMemcpyDeviceToHost));
-	//
-	// // Run Kernel b
-	// // checkCuda(cudaEventRecord(startEvent, 0));
-	// kernel_b<<<dimGrid, dimBlock>>>(d_odata, d_idata, work_per_thread);
-	// checkCuda(cudaGetLastError());
-	// // checkCuda(cudaEventRecord(stopEvent, 0));
-	// // checkCuda(cudaEventSynchronize(stopEvent));
-	// // checkCuda(cudaEventElapsedTime(&ms, startEvent, stopEvent));
-	// checkCuda(cudaMemcpy(h_odata, d_odata, data_points * sizeof(float), cudaMemcpyDeviceToHost));
-	//
-	//
-	// // Analyse
-	// // int is_correct = check_result(reference, h_odata, data_points);
-	// // if (is_correct != -1) {
-	// // 	printf("Wrong result:   h_idata[%d] = %.20f\n\n",is_correct, h_idata[is_correct]);
-	// // } else {
-	// // 	printf("Correct result after %.5f ms\n", ms);
-	// // }
-	//
-	// // Cleanup
-	// // checkCuda(cudaEventDestroy(startEvent));
-	// // checkCuda(cudaEventDestroy(stopEvent));
-	// checkCuda(cudaFree(d_idata));
-	// checkCuda(cudaFree(d_odata));
-	// free(h_idata);
-	// free(h_odata);
-	// free(reference);
 }
---
+++ test-kernels/microkernels/mk_calls.cuh
@@ -0,0 +1,6 @@
+#ifndef MK_CALLS_CUH
+#define MK_CALLS_CUH
+
+void run_kernel_a(int argc, char **argv);
+
+#endif
